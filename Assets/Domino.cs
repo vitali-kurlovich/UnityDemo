713856f99a699d1e55bf7b732461b122627ff4d2
Vitali Kurlovich <vitalikurlovich@gmail.com>
2017-03-23 16:45:16 +0300

    update

--- Assets/Domino.cs
+++ Assets/Domino.cs
@@ -2,36 +2,39 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Domino : MonoBehaviour {
+public class Domino : MonoBehaviour
+{
 
 
-public enum DominoType
-        {
-            Domino_0 = 0,
-            Domino_1 = 1,
-            Domino_2 = 2,
-            Domino_3 = 3,
-			Domino_4 = 4,
-			Domino_5 = 5,
-			Domino_6 = 6
-        }
+	public enum DominoType
+	{
+		Domino_0 = 0,
+		Domino_1 = 1,
+		Domino_2 = 2,
+		Domino_3 = 3,
+		Domino_4 = 4,
+		Domino_5 = 5,
+		Domino_6 = 6
+	}
 
- public DominoType dominoType;
+	public DominoType dominoType;
 	// Use this for initialization
-	void Start () {
-var go = Resources.Load("domino_66") as GameObject;
+	void Start ()
+	{
+		var go = Resources.Load<Mesh>("domino_low_66");
 
-Debug.Log(go);
+		Debug.Log (go);
 
-var resMeshFilter = go.GetComponent<MeshFilter>();
+		//var resMeshFilter = go.GetComponent<MeshFilter> ();
 
-var meshFilter = GetComponent<MeshFilter>();
-meshFilter.mesh = Instantiate(resMeshFilter.sharedMesh) as Mesh;
+		//var meshFilter = GetComponent<MeshFilter> ();
+		//meshFilter.mesh = Instantiate (resMeshFilter.sharedMesh) as Mesh;
 
 	}
 	
 	// Update is called once per frame
-	void Update () {
+	void Update ()
+	{
 		
 	}
 }
