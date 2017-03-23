8a731059cf578fccbfe4c26ffd2e2dd68239746f
Vitali Kurlovich <vitalikurlovich@gmail.com>
2017-03-23 13:56:13 +0300

    iphone 3dmodel

---
+++ Assets/WebCam.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WebCam : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		var camera = GetComponent<Camera>();
+		camera.aspect = 320.0f / 568.0f ;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
---
+++ Assets/WebCameraShake.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WebCameraShake : MonoBehaviour {
+
+private Transform webCameraTransform;
+
+private Vector3 position;
+private Quaternion rotation;
+	// Use this for initialization
+
+	void Start () {
+		webCameraTransform = GetComponent<Transform> ();
+		position = webCameraTransform.localPosition;
+		rotation = webCameraTransform.localRotation;
+	}
+
+
+	void FixedUpdate() {
+		var time =Time.fixedTime;
+		Vector3 pos = position;
+		pos.y += Mathf.Sin (time*2.53976f) * 0.165478f;
+		pos.x += Mathf.Sin (time*2.3564564f) * 0.12456f;
+
+		pos.z += Mathf.Sin (time*0.369564f) * 0.1187f;
+
+
+		var angles = rotation.eulerAngles;
+		angles.y +=  Mathf.Sin (time*0.169564f) * 3.3187f;
+		angles.z +=  Mathf.Sin (time*0.569564f) * 3.887f;
+		angles.x +=  Mathf.Sin (time*1.569564f) * 3.887f;
+
+		webCameraTransform.localPosition = pos;
+
+		webCameraTransform.localRotation = Quaternion.Euler(angles);
+	}
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
